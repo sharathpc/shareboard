0f2bdd0050f71c75c53fae451f7dd02afadcd2c9
sharath0033 <sharathchandra0033@gmail.com>
2022-09-02 14:16:52 +0530

    Services initial setup

--- webapp/src/redux/reducers/textEditor.ts
+++ webapp/src/redux/reducers/textEditor.ts
@@ -3,7 +3,7 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 export const textEditorSlice = createSlice({
     name: 'textEditor',
     initialState: {
-        value: ''
+        value: '{"root":{"children":[{"children":[{"detail":0,"format":0,"mode":"normal","style":"","text":"Enter some text...","type":"text","version":1}],"direction":"ltr","format":"","indent":0,"type":"paragraph","version":1}],"direction":"ltr","format":"","indent":0,"type":"root","version":1}}'
     },
     reducers: {
         setValue: (state, { payload }: PayloadAction<string>) => {
