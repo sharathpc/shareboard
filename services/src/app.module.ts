ad3b0432b279f0fd4318d9b1528d834f45babf41
sharath0033 <sharathchandra0033@gmail.com>
2022-09-02 14:21:29 +0530

    gateway boilerplate added

--- services/src/app.module.ts
+++ services/src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AppGateway } from './app.gateway';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  providers: [AppGateway, AppService]
 })
-export class AppModule {}
+export class AppModule { }
