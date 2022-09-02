d9f5ed1a67c4d09667e741167108bbcd654aca41
sharath0033 <sharathchandra0033@gmail.com>
2022-09-02 23:12:10 +0530

    Footer links target blank added

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -31,6 +31,7 @@ const SocialButton = ({
       cursor={'pointer'}
       as={'a'}
       href={href}
+      target={'_blank'}
       display={'inline-flex'}
       alignItems={'center'}
       justifyContent={'center'}
