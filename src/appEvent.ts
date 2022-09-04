df40698dcb120e7c703e95318d1251b3d2de5710
sharath0033 <sharathchandra0033@gmail.com>
2022-09-04 23:30:09 +0530

    Changes related toapp events

--- src/appEvent.ts
+++
@@ -1,8 +0,0 @@
-enum ChatEvent {
-    SendValue = 'send_value',
-    receiveValue = 'receive_value',
-    SendAllData = 'send_all_data',
-    ReceiveAllData = 'receive_all_data'
-}
-
-export default ChatEvent;
